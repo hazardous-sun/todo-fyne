22171ea229a621bae33bec7b055b75ff64c66017
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-10-03 23:36:19 -0300

    Added a function to connect to the database, to collect all data inside of it and to create new items todo.

--- database/crud.go
+++ database/crud.go
@@ -1,1 +1,57 @@
 package database
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/supabase-community/supabase-go"
+	"todolist.com/models"
+)
+
+func InitializeClient() *supabase.Client {
+	client, err := supabase.NewClient(API_URL, API_SERVICE_KEY, &supabase.ClientOptions{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return client
+}
+
+func Select(client *supabase.Client) []models.Todo {
+	data, count, err := client.From("todo").Select("*", "", false).Execute()
+
+	fmt.Println(count)
+
+	if err != nil {
+		panic(err)
+	}
+
+	todos := todoArrFromByteArr(data)
+
+	return todos
+}
+
+func todoArrFromByteArr(arr []byte) []models.Todo {
+	var todos []models.Todo
+	err := json.Unmarshal(arr, &todos)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return todos
+}
+
+func Create(client *supabase.Client, text string, check bool) {
+	todo := map[string]interface{}{
+		"description": text,
+		"checked":     check,
+	}
+	client.From("todo").Insert(
+		todo,
+		false,
+		"ERROR",
+		"TEST",
+		"0",
+	).Execute()
+}
