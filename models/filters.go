9890bb0b3ca343a75e1c5bd5c379bc06e14b997c
solaire <contact@unharmfulhazard.com>
2024-10-06 15:40:45 -0300

    Added some documentation for the functions dealing with the filters.

--- models/filters.go
+++ models/filters.go
@@ -1,12 +1,18 @@
+// Package models :
+// Contains the structs and functions for handling the filters used for selection in the database.
 package models
 
 import "fmt"
 
+// Filters :
+// Holds the data of which filters should be used
 type Filters struct {
 	Checked   bool
 	Unchecked bool
 }
 
+// NewFilters :
+// Initializes a Filters struct with all the flags set to false.
 func NewFilters() Filters {
 	return Filters{
 		Checked:   false,
@@ -14,6 +20,8 @@ func NewFilters() Filters {
 	}
 }
 
+// GetFlags :
+// Returns an array of booleans representing each flag state.
 func (f *Filters) GetFlags() []bool {
 	var flags []bool
 	flags = append(flags, f.Checked)
@@ -21,6 +29,8 @@ func (f *Filters) GetFlags() []bool {
 	return flags
 }
 
+// String :
+// Returns the formatted Filters instance as a string.
 func (f *Filters) String() string {
 	return fmt.Sprintf("{%v \t %v}", f.Checked, f.Unchecked)
 }
