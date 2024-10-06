237856d6a80f4e0a858be5075d59ebe25370b079
solaire <contact@unharmfulhazard.com>
2024-10-06 15:44:49 -0300

    Added some documentation for the functions dealing with the Todo items.

--- models/todo.go
+++ models/todo.go
@@ -1,16 +1,24 @@
+// Package models :
+// Contains the struct and functions for handling the todo items.
 package models
 
 import "fmt"
 
+// Todo :
+// A representation of an item in the list of things to do.
 type Todo struct {
 	Description string
 	Checked     bool
 }
 
+// NewTodo :
+// Initializes an unchecked item in the list.
 func NewTodo(description string) Todo {
 	return Todo{description, false}
 }
 
+// LoadTodo :
+// Builds an item to do in the list, that could be either true OR false.
 func LoadTodo(description string, checked bool) Todo {
 	return Todo{
 		description,
@@ -18,6 +26,8 @@ func LoadTodo(description string, checked bool) Todo {
 	}
 }
 
+// String :
+// Returns the formatted Todo instance as a string.
 func (t Todo) String() string {
 	return fmt.Sprintf("{%s \t %t}", t.Description, t.Checked)
 }
