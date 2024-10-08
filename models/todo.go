48bb13f5785b9db9373863320ff3f618b2d8fe70
solaire <141582502+hazardous-sun@users.noreply.github.com>
2024-10-08 10:27:03 -0300

    Added a new column 'title' for the database and had to update the Todo item model.

--- models/todo.go
+++ models/todo.go
@@ -7,20 +7,16 @@ import "fmt"
 // Todo :
 // A representation of an item in the list of things to do.
 type Todo struct {
+	Title       string
 	Description string
 	Checked     bool
 }
 
-// NewTodo :
-// Initializes an unchecked item in the list.
-func NewTodo(description string) Todo {
-	return Todo{description, false}
-}
-
 // LoadTodo :
 // Builds an item to do in the list, that could be either true OR false.
-func LoadTodo(description string, checked bool) Todo {
+func LoadTodo(title, description string, checked bool) Todo {
 	return Todo{
+		title,
 		description,
 		checked,
 	}
@@ -29,5 +25,5 @@ func LoadTodo(description string, checked bool) Todo {
 // String :
 // Returns the formatted Todo instance as a string.
 func (t Todo) String() string {
-	return fmt.Sprintf("{%s \t %t}", t.Description, t.Checked)
+	return fmt.Sprintf("{'%s' \t '%s' \t %t}", t.Title, t.Description, t.Checked)
 }
--- models/todo_test.go
+++ models/todo_test.go
@@ -2,32 +2,21 @@ package models
 
 import "testing"
 
-func TestNewTodo(t *testing.T) {
-	expected := Todo{
-		"test",
-		false,
-	}
-	received := NewTodo("test")
-
-	if received != expected {
-		t.Errorf("expected %v, got %v", expected, received)
-	}
-}
-
 func TestLoadTodo(t *testing.T) {
 	expected := Todo{
 		"test",
+		"test description",
 		true,
 	}
-	received := LoadTodo("test", true)
+	received := LoadTodo("test", "test description", true)
 	if received != expected {
 		t.Errorf("expected %v, got %v", expected, received)
 	}
 }
 
 func TestTodo_String(t *testing.T) {
-	expected := "{test \t false}"
-	received := NewTodo("test").String()
+	expected := "{'test' \t 'test description' \t false}"
+	received := LoadTodo("test", "test description", false).String()
 
 	if received != expected {
 		t.Errorf("expected %v, got %v", expected, received)
